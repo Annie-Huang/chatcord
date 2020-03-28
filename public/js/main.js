1836df3ccc75e1e3c72900985f9d9133295c9eea
Annie Huang <annie.hn@gmail.com>
2020-03-28 16:53:49 +1100

    Remove the two default msg we hard code into the chat.html. Changed the outputMessage method to use the message object information to display username|time|text

--- public/js/main.js
+++ public/js/main.js
@@ -43,10 +43,11 @@ function outputMessage(message) {
      */
     const div = document.createElement('div')
     div.classList.add('message');
+    // ${message}
     div.innerHTML = `
-        <p class="meta">Brad <span>9:12pm</span></p>
+        <p class="meta">${message.username} <span>${message.time}</span></p>
         <p class="text">
-            ${message}
+            ${message.text}
         </p>    
     `;
     chatMesssages.appendChild(div);
